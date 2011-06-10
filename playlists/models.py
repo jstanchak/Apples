ae3d5f0e6cd5bcec1062c2e46ad453eea50ac9b7
Jenny Stanchak <jennystanchak@gmail.com>
2011-06-10 16:10:18 -0400

    first big push

--- playlists/models.py
+++ playlists/models.py
@@ -1,19 +1,34 @@
 from django.db import models
+from django.forms import ModelForm
+from django.forms.models import modelformset_factory
 
 class Playlist(models.Model):
-	pl_name = models.CharField(maxlength=200)
-	date_created = models.DateTimeField('date created', auto_now_add=True)
-	date_edited = models.DateTimeField('date last edited', auto_now=True)
+	pl_name = models.CharField("Title",max_length=100)
+	date_created = models.DateTimeField('date created')
+	date_edited = models.DateTimeField('date last edited')
+	
+	def __unicode__(self):
+		return u'%s' % (self.pl_name)
+	
 
-	class Admin:
-		pass
+
+
+class PlaylistForm(ModelForm):
+	class Meta:
+		model = Playlist
+		exclude = ('date_created', 'date_edited')
+		
 
 class Song(models.Model):
-	artist = models.CharField(maxlength=200)
-	song_name = models.CharField('song name', maxlength=200)
-	bpm = models.IntegerField(blank=True)
-	length = models.DateTimeField('song length', blank=True)
-	playlist = models.ManyToManyField('Playlist', blank=True)
+	artist = models.CharField(max_length=200)
+	song_name = models.CharField('song name', max_length=200)
+	bpm = models.IntegerField(null=True, blank=True)
+	length = models.DateTimeField('song length', null=True, blank=True)
+	playlist = models.ManyToManyField('Playlist', null=True, blank=True)
 
-	class Admin:
-		pass	
+
+class SongForm(ModelForm):
+	class Meta:
+		model = Song
+
+SongFormSet = modelformset_factory(Song, extra=15)
--- playlists/views.py
+++ playlists/views.py
@@ -1,11 +1,17 @@
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
 from playlists.models import *
+
+
+import datetime
 
 def index(request):
 	latest_playlists = Playlist.objects.all().order_by('-date_edited')[:10]
-	return render_to_response('playlists/index.html', {'lastest_playlists': 
-latest_playlists})
+	all_songs = Song.objects.all().order_by('artist')
+	return render_to_response('playlists/index.html', {'latest_playlists': 
+latest_playlists, 'all_songs': all_songs})
 	
 def detail(request, playlist_id):
 	return HttpResponse("You're looking at playlist %s." % playlist_id)
@@ -14,8 +20,29 @@ def song_detail(request, song_id):
 	return HttpResponse("You're looking at song %s." % song_id)
 
 def add_playlist(request):
-	return HttpResponse("You're trying to add a playlist.")
+	if request.method == 'POST':
+		form = PlaylistForm(request.POST)
+		if request.POST:
+			pl_new = form.save(commit=False)
+			pl_new.date_created = datetime.datetime.now()
+			pl_new.date_edited = datetime.datetime.now()
+			pl_new.save()
+			return render_to_response('playlists/detail.html', {'playlist': pl_new})
+		else:
+			return HttpResponse("it didn't save!")
+	else:
+		form = PlaylistForm()
+		return render_to_response('playlists/add_list.html',{'form':form}, context_instance=RequestContext(request))
 
 def add_song(request):
-	return HttpResponse("You're trying to add a song.")
+	if request.method == 'POST':
+		form = SongForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/playlists/')
+		else:
+			return HttpResponse("it didn't save!")
+	else:
+		form = SongForm()
+		return render_to_response('playlists/add_song.html',{'form':form}, context_instance=RequestContext(request))
 
