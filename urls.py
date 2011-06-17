00800d5de598414a970ba22553f5313a2e314f38
Jenny Stanchak <jennystanchak@gmail.com>
2011-06-17 15:54:54 -0400

    map works on dev server

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls.defaults import *
+from django.views.static import *
+from django.conf import settings
 
 
 urlpatterns = patterns('',
+	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.MEDIA_ROOT}),
     (r'^playlists/', include('playlists.urls')),
 	(r'^map/', include('map.urls')),
     (r'^$', 'apples.views.home'),
