eb2072f0358464a654510be5687663e70188592c
Jenny Stanchak <jennystanchak@gmail.com>
2011-09-18 00:07:35 -0400

    chris is a genius

--- media/musicsearch/main.js
+++ media/musicsearch/main.js
@@ -10,13 +10,15 @@ function clearBox() {
 
 function pullJSON() {
 	var artist = document.getElementById("artist_search").value;
-	var numResults = 50
+	var numResults = 100
 	url = "http://developer.echonest.com/api/v4/artist/audio?"
 	$.getJSON(url+'api_key='+api_key+'&name='+artist+'&results='+numResults+'&format=jsonp&callback=?', {},
 		function(data) {
-			totalSongs = Math.max(data["response"]["total"], numResults);
-			var songPick = Math.floor(Math.random()*(totalSongs + 1));
+			totalSongs = Math.min(data["response"]["total"], numResults);
+			var songPick = Math.floor(Math.random()*(totalSongs));
+			alert(songPick);
 			var mUrl = data["response"]["audio"][songPick]["url"];
+
 			var info = data["response"]["audio"][songPick]["artist"]+" - "+data["response"]["audio"][songPick]["title"];
 			document.getElementById("response_goes_here").innerHTML = "<span style='color:white;font-size:40px;'>"+info+"</span";
 			document.getElementById("audio_player").setAttribute('src', mUrl);
